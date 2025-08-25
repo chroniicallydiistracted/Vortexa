505e0d09d0a7e4ae6750c79860cfab42a7ed4a77
chroniicallydiistracted <chroniicallydiistracted@gmail.com>
2025-08-25 01:10:59 -0700

    chore(tooling): add flat ESLint config and Prettier ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,19 @@
 module.exports = {
   root: true,
   ignorePatterns: ['dist','node_modules'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module', project: false },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   rules: {
     'no-restricted-imports': [
       'error',
       { patterns: ['../../*','../../../*'] }
-    ]
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 };
---
+++ eslint.config.cjs
@@ -0,0 +1,25 @@
+// ESLint flat config migrated from previous .eslintrc.cjs
+const ts = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts','**/*.tsx'],
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      'sam-installer/**'
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tsParser,
+    },
+    plugins: { '@typescript-eslint': ts },
+    rules: {
+      ...ts.configs.recommended.rules,
+      'no-restricted-imports': ['error', { patterns: ['../../*','../../../*'] }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  }
+];
